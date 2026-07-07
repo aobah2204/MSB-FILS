e0b9142a494352fcb4ae6084579eb008f6369568
Amadou Oury Bah <aobah34@gmail.com>
2026-07-07 21:23:32 +0200

    Détails véhicule

--- msb_fils/src/pages/Depenses.jsx
+++ msb_fils/src/pages/Depenses.jsx
@@ -112,6 +112,7 @@ function Depenses() {
           </tbody>
         </table>
       </div>
+      
     </div>
   );
 }
--- msb_fils/src/pages/VehicleDetails.jsx
+++ msb_fils/src/pages/VehicleDetails.jsx
@@ -40,9 +40,10 @@ function VehicleDetails(){
         if (!data) return alert("Aucun véhicule");
 
         setVehicule(data);
-        console.log(data);
 
         getChauffeur(data);
+        getLivraisonsVehicule(data);
+        getDepensesVehicule(data);
         
     }
 
@@ -56,15 +57,49 @@ function VehicleDetails(){
 
         if (!data) return alert("Aucun chauffeur");
 
-        setChauffeur(data);
-        console.log(chauffeur);
+        setChauffeur(data);        
+        //console.log(chauffeur);
 
+    }
+
+    // Get livraisons vehicules
+    const [livraisons, setLivraisons] = useState([]);
+    async function getLivraisonsVehicule(vehicule){
+
+        const { data } = await supabase
+            .from("livraisons")            
+            .select("*")
+            .eq("vehicule_id",vehicule?.id);
+
+        if (!data) return alert("Aucune livraison effectuée");
+        setLivraisons(data);
+    }
+
+
+    // Get depenses véhicules
+    const [depenses, setDepenses] = useState([]);
+    async function getDepensesVehicule(vehicule){
+
+        const { data } = await supabase
+            .from("depenses")            
+            .select("*")
+            .eq("vehicule_id",vehicule.id);
+
+        if (!data) return alert("Aucune dépense effectuée");
+        setDepenses(data);
+    }
+
+    function formatDate(value) {
+        if (!value) return "—";
+
+        const date = new Date(value);
+        return Number.isNaN(date.getTime()) ? value : date.toLocaleDateString("fr-FR");
     }
 
     // s'exécute une seule fois au chargement
     useEffect(() => {
 
-        getVehicule();
+        getVehicule();        
 
     }, []);
 
@@ -76,94 +111,129 @@ function VehicleDetails(){
                 Fiche véhicule #{id}
             </h1>
 
-            <div className="card">
 
-            <h3>
-                Informations
-            </h3>
+            <div className="cards">
 
-            <p>
-                {vehicule?.marque}
-            </p>
+                <div className="card">
 
+                    <h3>
+                        Informations
+                    </h3>
 
-            <p>
-                {vehicule?.immatriculation}
-            </p>
+                    <p>
+                        {vehicule?.marque}
+                    </p>
 
 
-            <p>
-                Kilométrage : {vehicule.kilometrage} km
-            </p>
+                    <p>
+                        {vehicule?.immatriculation}
+                    </p>
 
 
-        </div>
+                    <p>
+                        Kilométrage : {vehicule.kilometrage} km
+                    </p>
+                </div>
+
+                <div className="card">
 
 
-
-        <div className="card">
-
-
-            <h3>
-                Chauffeur
-            </h3>
+                    <h3>
+                        Chauffeur
+                    </h3>
 
 
-            <p>
-                {chauffeur?.fullname}
-            </p>
+                    <p>
+                        {chauffeur?.fullname}
+                    </p>
+
+                    <p>
+                        {chauffeur?.telephone}
+                    </p>
+                    <p>
+                        {chauffeur?.adresse}
+                    </p>
 
 
-        </div>
-
-
-
-
-        <div className="card">
-
-
-            <h3>
-                Maintenance
-            </h3>
-
-
-            <ul>
-
-                <li>
-                    Vidange - 80000 km
-                </li>
-
-                <li>
-                    Pneus - 75000 km
-                </li>
-
-            </ul>
-
-
-        </div>
-
-
-
-
-        <div className="card">
-
-
-            <h3>
-                Consommation
-            </h3>
-
-
-            <p>
-                Diesel : 9.5 L / 100 km
-            </p>
-
-
-            <p>
-                Dernier plein : 15/06/2026
-            </p>
-
+                </div>
 
             </div>
+
+            <br/>
+            
+            <div className="cards">                
+                <div className="table-container card" >
+                    <h3>Total des livraisons</h3>
+                    <table className="data-table">
+                        <thead className="headerTable">
+
+                        <tr className="header_Table">
+                            <th>Reférence</th>
+                            <th>Vente</th>
+                            <th>Date</th>
+                            <th>Adresse</th> 
+                            <th>Montant</th>
+                        </tr>
+
+                        </thead>
+
+
+                        <tbody>
+
+                        {livraisons.map((livraison, index) => (
+
+                            <tr key={index}>
+
+                            <td>{livraison.reference}</td>
+
+                            <td>{livraison.vente_id || "--"}</td>
+
+                            <td>{livraison?.date_livraison.split('T')[0]}</td>
+
+                            <td>{livraison.addresse}</td>
+
+                            <td>{new Intl.NumberFormat("fr-FR").format(livraison.montant) || 0} FG</td>  
+
+                        </tr>
+
+                        ))
+                        }
+
+                    </tbody>
+                    </table>
+                </div>
+
+                <div className="table-container card">
+                    <h3>Total des dépenses</h3>
+                    <table className="data-table">
+                        <thead className="header_Table">
+                            <tr>
+                            <th>Référence</th>
+                            <th>catégorie</th>
+                            <th>Date</th>
+                            <th>Montant total</th>
+                            <th>Statut</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {depenses.map((Depense) => (
+                            <tr key={Depense.id}>
+                                <td>{Depense.reference || "—"}</td>
+                                <td>{Depense.categorie}</td>
+                                <td>{formatDate(Depense.date_depense) || "—"}</td>
+                                <td>{new Intl.NumberFormat("fr-FR").format(Depense.montant) || 0 } FG</td>
+                                <td>{Depense.statut || "—"}</td>                            
+                            </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div> 
+                
+
+            </div>
+
+            
+
 
 
         </div>
