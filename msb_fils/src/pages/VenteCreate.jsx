90d195241bf0365fe779de1242cc57a66e2b1cef
Amadou Oury Bah <aobah34@gmail.com>
2026-07-11 11:32:26 +0200

    Ajout marchandise dans ventes

--- msb_fils/src/pages/VenteCreate.jsx
+++ msb_fils/src/pages/VenteCreate.jsx
@@ -6,6 +6,7 @@ import { useAuth } from "../context/AuthContext";
 import Select from "react-select";
 
 import { selectStyle } from "../components/selectStyle";
+import Marchandises from "./Marchandises";
 
 function VenteCreate() {
   const navigate = useNavigate();
@@ -23,20 +24,27 @@ function VenteCreate() {
 
   const [clients, setClients] = useState([]);
   const [products, setProducts] = useState([]);
+  const [marchandises, setMarchandises] = useState([]);
   const [productLines, setProductLines] = useState([]);
+  const [marchandiseLines, setMarchandiseLines] = useState([]);
   const [sites, setSites] = useState([]);
+  const [fournisseurs, setFournisseurs] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
 
   async function loadOptions() {
-    const [{ data: clientsData }, { data: productsData }, {data: sitesData}] = await Promise.all([
+    const [{ data: clientsData }, { data: productsData }, { data: sitesData }, { data: marchandiseData }, { data: fournisseursData}] = await Promise.all([
       supabase.from("clients").select("id, nom, prenom"),
       supabase.from("products").select("id, nom, categorie, prixVente"),
       supabase.from("siteproduction").select("id, nom, adresse"),
+      supabase.from("marchandises").select("id, nom, categorie, description"),
+      supabase.from("fournisseurs").select("id, nom, prenom, societe"),
     ]);
 
     setClients(clientsData || []);
+    setFournisseurs(fournisseursData || []);
     setProducts(productsData || []);
     setSites(sitesData || []);
+    setMarchandises(marchandiseData || []);
   }
 
   useEffect(() => {
@@ -44,11 +52,14 @@ function VenteCreate() {
   }, []);
 
   useEffect(() => {
-    const total = productLines.reduce((sum, line) => {
+    const totalP = productLines.reduce((sum, line) => {
       return sum + (Number(line.quantite || 0) * Number(line.prix_unitaire || 0));
     }, 0);
-    setTotalAmount(total);
-  }, [productLines]);
+    const totalM = marchandiseLines.reduce((sum, line) => {
+      return sum + (Number(line.quantite || 0) * Number(line.prix_unitaire || 0));
+    }, 0);
+    setTotalAmount(totalP + totalM);
+  }, [productLines, marchandiseLines]);
 
   function handleFormChange(e) {
     const { name, value } = e.target;
@@ -61,6 +72,18 @@ function VenteCreate() {
       {
         produit_id: "",
         site_id: "",
+        quantite: 0,
+        prix_unitaire: 0,
+      },
+    ]);
+  }
+
+  function addMarchandiseLine() {
+    setMarchandiseLines([
+      ...marchandiseLines,
+      {
+        marchandise_id: "",
+        fournisseur_id: "",
         quantite: 0,
         prix_unitaire: 0,
       },
@@ -92,6 +115,31 @@ function VenteCreate() {
       setProductLines(copy);
   }
 
+  function updateMarchandiseLine(index, field, value) {
+      const copy = [...marchandiseLines];
+
+      const numValue =
+          field !== "marchandise_id" && field !== "fournisseur_id"
+              ? Number(value)
+              : value;
+
+      copy[index][field] = numValue;
+
+      // Si le marchandise ou le fournisseur change, on recherche le bon produit
+      if (field === "marchandise_id" || field === "fournisseur_id") {
+
+          const selectedMarchandise= marchandises.find(
+              (p) =>
+                  String(p.id) === String(copy[index].marchandise_id) &&
+                  String(p.site_id) === String(copy[index].fournisseur_id)
+          );
+
+          copy[index].prix_unitaire = Number(selectedMarchandise?.prixVente ?? 0);
+      }
+
+      setMarchandiseLines(copy);
+  }
+
   function updateSiteLine(field, value) {
     const copy = [...productLines];
     const numValue = field !== "site_id" ? Number(value) : value;
@@ -105,8 +153,25 @@ function VenteCreate() {
     setSite(copy);
   }
 
+  function updateFournisseurLine(field, value) {
+    const copy = [...marchandiseLines];
+    const numValue = field !== "fournisseur_id" ? Number(value) : value;
+
+    copy[field] = numValue;
+
+    if(field === "fournisseur_id"){
+      const selectedFour = fournisseurs.find((p) => String(p.id) === String(value));
+    }
+
+    setFournisseur(copy);
+  }
+
   function removeProductLine(index) {
     setProductLines(productLines.filter((_, i) => i !== index));
+  }
+
+  function removeMarchandiseLine(index) {
+    setMarchandiseLines(marchandiseLines.filter((_, i) => i !== index));
   }
 
   async function handleSubmit(e) {
@@ -143,6 +208,7 @@ function VenteCreate() {
       return;
     }
 
+    // Ajout produits ventes
     for (const line of productLines) {
       if (!line.produit_id || !line.quantite) continue;
 
@@ -163,12 +229,40 @@ function VenteCreate() {
       }
     }
 
+    // Ajout marchandise ventes 
+    for (const line of marchandiseLines) {
+      if (!line.marchandise_id || !line.quantite) continue;
+
+      const linePayload = {
+        vente_id: insertedVente.id,
+        marchandise_id: line.marchandise_id,
+        fournisseur_id: line.fournisseur_id,
+        quantite: Number(line.quantite),
+        prix_unitaire: Number(line.prix_unitaire),
+        montant_ligne: Number(line.quantite) * Number(line.prix_unitaire),
+      };
+
+      const { error: lineError } = await supabase.from("ventemarchandises").insert(linePayload);
+
+      if (lineError) {
+        alert("Erreur lors de l'ajout des marchandises de la ventes : " + lineError.message);
+        return;
+      }
+    }
+
     alert("Vente enregistrée");
     navigate("/ventes");
   }
 
   // Select filter and style 
   const productOptions = products.map((p) => ({
+      value: p.id,
+      label: `${p.nom} - ${p.categorie}`,
+      product: p
+  }));
+
+  // Select filter and style 
+  const marchandiseOptions = marchandises.map((p) => ({
       value: p.id,
       label: `${p.nom} - ${p.categorie}`,
       product: p
@@ -285,6 +379,92 @@ function VenteCreate() {
 
         <button className="profile" type="button" onClick={addProductLine}>
           + Ajouter produit
+        </button>
+
+        <h3>Marchandises</h3>
+
+        {marchandiseLines.map((line, index) => (
+          <div className="grid" key={index} style={{ marginBottom: "20px", gap: "10px" }}>
+            <div>
+                <label>Marchandise</label>
+
+                <Select className="list_select"
+                    options={marchandiseOptions}
+
+                    placeholder="Choisir une marchandise..."
+
+                    isSearchable
+
+                    styles={selectStyle}
+
+                    value={
+                        marchandiseOptions.find(
+                            option => option.value === line.marchandise_id
+                        ) || null
+                    }
+
+                    onChange={(selected) => {
+
+                        updateMarchandiseLine(
+                            index,
+                            "marchandise_id",
+                            selected.value
+                        );
+
+                    }}
+                />
+            </div> 
+            <div>
+              <label>Fournisseur</label>
+              <select name="fournisseur_id" value={form.fournisseur_id} 
+                value={line.fournisseur_id || ""}
+                onChange={(e) => updateMarchandiseLine(index, "fournisseur_id", e.target.value)}
+                required>
+                <option value="">Choisir le fournisseur</option>
+                {fournisseurs.map((fournisseur) => (
+                  <option key={fournisseur.id} value={fournisseur.id}>{fournisseur.nom} {fournisseur.nom} - {fournisseur.societe}</option>
+                ))}
+              </select>
+            </div>           
+
+            <div>
+              <label>Quantité</label>
+              <input
+                type="number"
+                value={line.quantite || ""}
+                onChange={(e) => updateMarchandiseLine(index, "quantite", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>Prix unitaire</label>
+              <input
+                type="number"
+                onChange={(e) => updateMarchandiseLine(index, "prix_unitaire", e.target.value)}
+              />
+            </div>
+
+            <div>
+              <label>Total ligne</label>
+              <output>
+                {new Intl.NumberFormat("fr-FR").format(Number(line.quantite || 0) * Number(line.prix_unitaire || 0))} FG
+              </output>
+            </div>
+
+            <div style={{ display: "flex", alignItems: "flex-end" }}>
+              <button
+                className="profileSupp"
+                type="button"
+                onClick={() => removeMarchandiseLine(index)}
+              >
+                <Trash2 size={20} />
+              </button>
+            </div>
+          </div>
+        ))}
+
+        <button className="profile" type="button" onClick={addMarchandiseLine}>
+          + Ajouter marchandise
         </button>
 
         <div className="profile" style={{ backgroundColor: "#a8415b"}}>
