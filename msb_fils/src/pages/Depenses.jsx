449db17a3a453d10f85004da010fad85a222dda9
Amadou Oury Bah <aobah34@gmail.com>
2026-07-23 18:14:51 +0200

    Filtre depenses

--- msb_fils/src/pages/Depenses.jsx
+++ msb_fils/src/pages/Depenses.jsx
@@ -1,4 +1,4 @@
-import { Eye, Pencil, Trash2, Package } from "lucide-react";
+import { Eye, Pencil, Trash2, Package, Search, Filter, RotateCcw } from "lucide-react";
 import { NavLink } from "react-router-dom";
 import { supabase } from "../supabase.js";
 import { useState, useEffect } from "react";
@@ -7,21 +7,49 @@ import { useAuth } from "../context/AuthContext";
 
 function Depenses() {
   const { user } = useAuth();
-  const [DepensesList, setDepensesList] = useState([]);
+  const [sites, setSites] = useState([]);
+  const [fournisseurs, setFournisseurs] = useState([]);
+  const [vehicules, setVehicules] = useState([]);
+  const [depenses, setDepenses] = useState([]);
+  const [depensesFiltrees, setDepensesFiltrees] = useState([]);
 
   useEffect(() => {
     loadDepenses();
-  }, []);
+}, []);
 
   async function loadDepenses() {
     try {
-      const { data } = await supabase
+      const { data: depensesData } = await supabase
         .from("depenses")
         .select("*")
         .order("date_depense", { ascending: false });
 
-      if (!data) return alert("Aucun Dépense");
-      setDepensesList(data);
+      if (!depensesData) return alert("Aucun Dépense");
+      setDepenses(depensesData);
+      setDepensesFiltrees(depensesData);
+
+      const { data: sitesData } = await supabase
+        .from("siteproduction")
+        .select("*");
+
+      if (!sitesData) return alert("Aucun site");
+      setSites(sitesData);
+
+      const { data: fournisseursData } = await supabase
+        .from("fournisseurs")
+        .select("*");
+
+      if (!fournisseursData) return alert("Aucun fournisseur");
+      setFournisseurs(fournisseursData);
+
+      const { data: vehiculesData } = await supabase
+        .from("vehicules")
+        .select("*");
+
+      if (!vehiculesData) return alert("Aucun véhicule");
+      setVehicules(vehiculesData);
+
+
     } catch (error) {
       console.error("Erreur lors du chargement des Dépenses :", error);
     }
@@ -46,8 +74,570 @@ function Depenses() {
     return Number.isNaN(date.getTime()) ? value : date.toLocaleDateString("fr-FR");
   }
 
+  const [filters, setFilters] = useState({
+      search: "",
+      dateDebut: "",
+      dateFin: "",
+      categorie: "",
+      site: "",
+      fournisseur: "",
+      vehicule: "",
+      modePaiement: "",
+      statut: "",
+      montantMin: "",
+      montantMax: ""
+  });
+
+  const handleFilterChange = (e) => {
+      const { name, value } = e.target;
+
+      setFilters(prev => ({
+          ...prev,
+          [name]: value
+      }));
+  };
+
+  const depensesFiltres = depenses.filter(depense => {
+
+      const date = new Date(depense.date_depense);
+
+      return (
+
+          (!filters.search ||
+              depense.libelle.toLowerCase().includes(filters.search.toLowerCase()) ||
+              depense.reference?.toLowerCase().includes(filters.search.toLowerCase()))
+
+          &&
+
+          (!filters.categorie ||
+              depense.categorie === filters.categorie)
+
+          &&
+
+          (!filters.site ||
+              depense.site_id === filters.site)
+
+          &&
+
+          (!filters.fournisseur ||
+              depense.fournisseur_id === filters.fournisseur)
+
+          &&
+
+          (!filters.vehicule ||
+              depense.vehicule_id === filters.vehicule)
+
+          &&
+
+          (!filters.modePaiement ||
+              depense.mode_paiement === filters.modePaiement)
+
+          &&
+
+          (!filters.statut ||
+              depense.statut === filters.statut)
+
+          &&
+
+          (!filters.dateDebut ||
+              date >= new Date(filters.dateDebut))
+
+          &&
+
+          (!filters.dateFin ||
+              date <= new Date(filters.dateFin + "T23:59:59"))
+
+          &&
+
+          (!filters.montantMin ||
+              Number(depense.montant) >= Number(filters.montantMin))
+
+          &&
+
+          (!filters.montantMax ||
+              Number(depense.montant) <= Number(filters.montantMax)))
+  });
+
+  const reinitialiser = () => {
+      const initialFilters = {
+        search: "",
+        dateDebut: "",
+        dateFin: "",
+        categorie: "",
+        site: "",
+        fournisseur: "",
+        vehicule: "",
+        modePaiement: "",
+        statut: "",
+        montantMin: "",
+        montantMax: ""
+    };
+
+    setFilters(initialFilters);
+    setDepensesFiltrees(depenses);
+  };
+
+
+  const rechercher = () => {
+
+    console.log("In recherche ...", filters, depenses);
+
+      const resultat = depenses.filter((depense) => {
+
+          // Recherche texte
+          const rechercheOK =
+              filters.search === "" ||
+
+              depense.libelle?.toLowerCase().includes(filters.search.toLowerCase()) ||
+
+              depense.reference?.toLowerCase().includes(filters.search.toLowerCase());
+
+          // Catégorie
+          const categorieOK =
+              filters.categorie === "" ||
+              depense.categorie === filters.categorie;
+
+          // Site
+          const siteOK =
+              filters.site === "" ||
+              depense.site_id == filters.site;
+
+          // Fournisseur
+          const fournisseurOK =
+              filters.fournisseur === "" ||
+              depense.fournisseur_id == filters.fournisseur;
+
+          // Véhicule
+          const vehiculeOK =
+              filters.vehicule === "" ||
+              depense.vehicule_id == filters.vehicule;
+
+          // Paiement
+          const paiementOK =
+              filters.modePaiement === "" ||
+              depense.mode_paiement === filters.modePaiement;
+
+          // Statut
+          const statutOK =
+              filters.statut === "" ||
+              depense.statut === filters.statut;
+
+          // Date
+          const date = new Date(depense.date_depense);
+
+          const dateDebutOK =
+              filters.dateDebut === "" ||
+              date >= new Date(filters.dateDebut);
+
+          const dateFinOK =
+              filters.dateFin === "" ||
+              date <= new Date(filters.dateFin + "T23:59:59");
+
+          // Montants
+          const montantMinOK =
+              filters.montantMin === "" ||
+              Number(depense.montant) >= Number(filters.montantMin);
+
+          const montantMaxOK =
+              filters.montantMax === "" ||
+              Number(depense.montant) <= Number(filters.montantMax);
+
+          return (
+              rechercheOK &&
+              categorieOK &&
+              siteOK &&
+              fournisseurOK &&
+              vehiculeOK &&
+              paiementOK &&
+              statutOK &&
+              dateDebutOK &&
+              dateFinOK &&
+              montantMinOK &&
+              montantMaxOK
+          );
+
+      });
+
+      console.log("Resultat ", resultat);
+
+      setDepensesFiltrees(resultat);
+
+  };
+
   return (
     <div className="product-page">
+
+      <div className="bg-white rounded-xl shadow-md p-5 mb-5">
+
+            <div className="flex items-center gap-2 mb-4">
+                <Filter size={22} />
+                <h2 className="text-xl font-semibold">
+                    Filtres
+                </h2>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+
+                {/* Recherche */}
+
+                <div>
+                    <label className="text-sm font-medium">
+                        Recherche
+                    </label>
+
+                    <div className="relative">
+
+                        <Search
+                            size={18}
+                            className="absolute left-3 top-3 text-gray-400"
+                        />
+
+                        <input
+                            type="text"
+                            name="search"
+                            value={filters.search}
+                            onChange={handleFilterChange}
+                            placeholder="Libellé ou référence..."
+                            className="w-full pl-10 p-2 border rounded-lg"
+                        />
+
+                    </div>
+                </div>
+
+                {/* Date début */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Du
+                    </label>
+
+                    <input
+                        type="date"
+                        name="dateDebut"
+                        value={filters.dateDebut}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    />
+
+                </div>
+
+                {/* Date fin */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Au
+                    </label>
+
+                    <input
+                        type="date"
+                        name="dateFin"
+                        value={filters.dateFin}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    />
+
+                </div>
+
+                {/* Catégorie */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Catégorie
+                    </label>
+
+                    <select
+                        name="categorie"
+                        value={filters.categorie}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">Toutes</option>
+
+                        {/*{categories.map(cat => (
+
+                            <option
+                                key={cat}
+                                value={cat}
+                            >
+                                {cat}
+                            </option>
+
+                        ))}*/}
+            
+                
+                      <option>
+                          Carburant
+                      </option>
+                      <option>
+                          Transport
+                      </option>
+                      <option>
+                          Réparation
+                      </option>
+                      <option>
+                          Pièce
+                      </option>
+                      <option>
+                          Salaire
+                      </option>
+                      <option>
+                          Electricité
+                      </option>
+                      <option>
+                          Eau
+                      </option>
+                      <option>
+                          Loyer
+                      </option>
+                      <option>
+                          Transport
+                      </option>
+                      <option>
+                          Matières premières
+                      </option>
+                      <option>
+                          Consommables
+                      </option>
+                      <option>
+                          Impôts
+                      </option>
+                      <option>
+                          Assurance
+                      </option>
+                      <option>
+                          Fournisseur
+                      </option>
+                      <option>
+                          Prime de voyage
+                      </option>
+                      <option>
+                          Frais de route
+                      </option>
+                      <option>
+                          Manutention
+                      </option>                
+                      <option>
+                          Divers
+                      </option>
+
+
+                    </select>
+
+                </div>
+
+                {/* Site */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Site
+                    </label>
+
+                    <select
+                        name="site"
+                        value={filters.site}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">
+                            Tous
+                        </option>
+
+                        {sites.map(site => (
+
+                            <option
+                                key={site.id}
+                                value={site.id}
+                            >
+                                {site.nom}
+                            </option>
+
+                        ))}
+
+                    </select>
+
+                </div>
+
+                {/* Fournisseur */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Fournisseur
+                    </label>
+
+                    <select
+                        name="fournisseur"
+                        value={filters.fournisseur}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">
+                            Tous
+                        </option>
+
+                        {fournisseurs.map(f => (
+
+                            <option
+                                key={f.id}
+                                value={f.id}
+                            >
+                                {f.nom} {f.prenom} - {f.societe}
+                            </option>
+
+                        ))}
+
+                    </select>
+
+                </div>
+
+                {/* Véhicule */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Véhicule
+                    </label>
+
+                    <select
+                        name="vehicule"
+                        value={filters.vehicule}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">
+                            Tous
+                        </option>
+
+                        {vehicules.map(v => (
+
+                            <option
+                                key={v.id}
+                                value={v.id}
+                            >
+                                {v.immatriculation} - {v.chauffeur}
+                            </option>
+
+                        ))}
+
+                    </select>
+
+                </div>
+
+                {/* Paiement */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Paiement
+                    </label>
+
+                    <select
+                        name="modePaiement"
+                        value={filters.modePaiement}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">Tous</option>
+                        <option>Cash</option>
+                        <option>Virement</option>
+                        <option>Chèque</option>
+                        <option>Orange Money</option>
+                        <option>Carte bancaire</option>
+
+                    </select>
+
+                </div>
+
+                {/* Statut */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Statut
+                    </label>
+
+                    <select
+                        name="statut"
+                        value={filters.statut}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    >
+
+                        <option value="">Tous</option>
+                        <option>Payé</option>
+                        <option>Non payé</option>
+                    </select>
+
+                </div>
+
+                {/* Montant min */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Montant min
+                    </label>
+
+                    <input
+                        type="number"
+                        name="montantMin"
+                        value={filters.montantMin}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    />
+
+                </div>
+
+                {/* Montant max */}
+
+                <div>
+
+                    <label className="text-sm font-medium">
+                        Montant max
+                    </label>
+
+                    <input
+                        type="number"
+                        name="montantMax"
+                        value={filters.montantMax}
+                        onChange={handleFilterChange}
+                        className="w-full p-2 border rounded-lg"
+                    />
+
+                </div>
+
+            </div>
+
+            {/* Boutons */}
+
+            <div className="grid">
+
+                <button className="profile"
+                    onClick={rechercher}
+                >
+                    Filtrer
+                </button>
+
+                <button
+                    onClick={reinitialiser}
+                    className="profile"
+                >
+                    <RotateCcw size={18} />
+                    Réinitialiser
+                </button>
+
+            </div>
+
+        </div>
 
       <h1>Liste des Dépenses</h1>
       {["Administrateur", "Responsable de production", "Superviseur", "Coordinateur", "Commercial"].includes(
@@ -82,7 +672,7 @@ function Depenses() {
             </tr>
           </thead>
           <tbody>
-            {DepensesList.map((Depense) => (
+            {depensesFiltrees.map((Depense) => (
               <tr key={Depense.id}>
                 <td>{Depense.reference || "—"}</td>
                 <td>{Depense.categorie}</td>
@@ -96,12 +686,12 @@ function Depenses() {
                   user?.role
                 ) && (
                   <td>
-                    <NavLink to={`/Depenses/details/${Depense.id}`}>
+                    <NavLink to={`/depenses/details/${Depense.id}`}>
                       <button className="profile">
                         <Eye size={20} />
                       </button>
                     </NavLink>
-                    {/*<NavLink to={`/Depenses/modifier/${Depense.id}`}>
+                    {/*<NavLink to={`/depenses/modifier/${Depense.id}`}>
                       <button className="profile">
                         <Pencil size={20} />
                       </button>
